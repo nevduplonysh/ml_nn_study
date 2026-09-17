f35b27387b25af8a9d9a4c2729a214132fc36841
nevduplonysh <nevduplonysh@gmail.com>
2026-09-17 19:33:16 +0300

    removed one comment

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -5,7 +5,7 @@ class Neuron:
 
     def __init__(self, nin):
         self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
-        self.b = Value(random.uniform(-1,1)) # -1 включаем а 1 исключаем?
+        self.b = Value(random.uniform(-1,1))
 
     def __call__(self, x):
         act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
